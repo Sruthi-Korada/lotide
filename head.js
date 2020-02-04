b798b27cb53ec5c91e9eb73dbad48ba01a8591fd
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-04 17:05:03 +0000

    head

---
+++ head.js
@@ -0,0 +1,17 @@
+var array = [];
+var num = 0;
+var assertEqual = function head (array , num){
+    
+   var array = array[i];
+  while (i = 0){
+      if (array [i] === num){
+          console.log ("true");
+
+      }else {
+          console.log('fail');
+      }
+  }
+}
+    
+        assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
