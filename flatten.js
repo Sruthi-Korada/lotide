64e2eb3f9e30b5f7fa337bf0b2f6566be542b250
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-05 04:14:03 +0000

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,47 @@
+const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1, array2)) {
+      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+    }
+  };
+  
+  module.exports = assertArraysEqual;
+    
+  const eqArrays = function(array1, array2) {
+      if (array1.length !== array2.length) {
+        return false;
+      }
+      for (let i = 0; i < array1.length; i++) {
+        if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+          // if it is an array && the arrays are not equal then return false;
+          return false;
+        }
+        if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+          // if the element is not an array and the items are not equal then return false
+          return false;
+        }
+      } 
+      return true;
+    };
+    
+    module.exports = eqArrays;
+
+    const flatten = function(array){
+        let output = [];
+        for (i = 0; i < array.length; i ++){
+            if (Array.isArray(array[i]) ){
+                for(j=0; j < array[i].length; j++){
+                    output.push(array[i][j]);
+                }
+            } 
+            else {
+                 output.push(array[i]);
+            }
+           
+        }
+        return output;
+    }
+
+    module.exports = flatten;
+    console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
