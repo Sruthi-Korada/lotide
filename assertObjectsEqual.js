7ae2d050a520028df664d27cb6da7944fddeb3c5
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-06 02:15:39 +0000

    esObjects with assertion  usage of object.keys

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+
+  
+  const eqObjects = function(object1, object2) {
+    let objectListOne = Object.keys(object1);
+    let objectListTwo = Object.keys(object2);
+    if (objectListOne.length !== objectListTwo.length) {
+      return false;
+    } else {
+      for (let key of objectListOne) {
+        if (object1[key] === object2[key]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+  
+  const assertObjectsEqual = function(actual, expected) {
+    const inspect = require('util').inspect;
+    if (eqObjects(actual,expected)){
+        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    }
+    else{
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    }
+  };
+  
+  
+  
+  const ab = { a: "1", b: "2" };
+  const ba = { b: "2", a: "1" };
+  console.log(assertObjectsEqual(ab, ba)); // => true
+  
+  const abc = { a: "1", b: "2", c: "3" };
+  console.log(assertObjectsEqual(ab, abc)); // => false
+  
+  assertObjectsEqual(eqObjects(ab, ba), true);
+  assertObjectsEqual(eqObjects(ab, abc), false);
