a663bd934b0a87e6bc48867d720dae6a99afa9d7
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-10 22:11:32 +0000

    refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -7,7 +7,5 @@ const assertEqual = function(actual, expected) {
   }
 };
         
-          
+module.exports = assertEqual;
          
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
