5f008707dd78f6c328b49d7712bbbfb9345ba7d2
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-11 02:30:16 +0000

    exporting all functions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual
 }
