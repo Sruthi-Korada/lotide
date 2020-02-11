ee49bd5d7e6b4a8103216690e79f0d7666ce44ae
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-11 00:39:12 +0000

    refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,35 +1,19 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let resultOne = '', resultTwo = '';
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      resultOne += arrOne[i];
+      resultTwo += arrTwo[i];
+    }
   } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    return (console.log('❌❌❌ ' + arrOne + ' !== ' + arrTwo));
+    
+  }
+  if (resultOne === resultTwo) {
+    console.log('✅ ' + arrOne + ' === ' + arrTwo);
+  } else {
+    console.log('🛑 ' + arrOne + ' !== ' + arrTwo);
   }
 };
-  
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
-      return false;
-    }
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-        // if it is an array && the arrays are not equal then return false;
-        return false;
-      }
-      if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-        // if the element is not an array and the items are not equal then return false
-        return false;
-      }
-    } 
-    return true;
-  };
-
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-    }
-  };
-  
-
-  
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,39 +1,14 @@
+const assertArraysEqual = require('./assertArraysEqual');
 
-
-  
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
-      return false;
+const middle = function(arr) {
+  let arrResult = [];
+  if (arr.length >= 2) {//only if array length > 2
+    if (arr.length % 2 === 0) {//for even array length
+      arrResult.push(arr[((arr.length) / 2) - 1]);
+      arrResult.push(arr[(arr.length) / 2]);
+    } else {//for odd array length
+      arrResult.push(arr[Math.floor(0.5 * arr.length)]);
     }
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-        // if it is an array && the arrays are not equal then return false;
-        return false;
-      }
-      if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-        // if the element is not an array and the items are not equal then return false
-        return false;
-      }
-    } 
-    return true;
-  };
-
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-    }
-  };
-  
-const middle = function(array) {
-    if (array.length <= 2) {
-      return [];
-    } else if (array.length % 2 === 0) {
-      return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
-    } else {
-      return [array[Math.floor(array.length / 2)]];
-    }
-  };
- console.log(middle([1, 2, 3]));
- 
+  } return arrResult;
+};
+module.exports = middle;
